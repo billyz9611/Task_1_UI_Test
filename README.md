5148927e71f4504f222134ca52fad9d8509341d8
东林 <57386266@qq.com>
2024-03-18 21:26:35 +0800

    add README file

---
+++ README.md
@@ -0,0 +1,9 @@
+## Install requirements
+- CMD : `pip3 install -r requirements.txt`
+
+## Install WebDrivers
+- CMD : `sbase install chromedriver`
+
+## Run behave test cases
+- CMD : `behave ./behave_test_cases`
+
--- behave_test_cases/steps/markets.py
+++ behave_test_cases/steps/markets.py
@@ -1,4 +1,5 @@
 from behave import step
+from seleniumbase import BaseCase
 
 
 @step("Open the Markets Page")
@@ -9,17 +10,19 @@ def go_to_markets(context):
 
 @step("Click {tab_name} Tab")
 def click_tab(context, tab_name):
-    sb = context.sb
-    sb.click('//span[text()="%s"]' % tab_name)
+    sb: BaseCase = context.sb
+    selector_txt = '//span[text()="%s"]' % tab_name
+    sb.wait_for_element(selector_txt)
+    sb.click(selector_txt)
 
 
 @step("Show {trade_coin_name:S}")
 def check_trade_items(context, trade_coin_name):
-    sb = context.sb
+    sb: BaseCase = context.sb
     sb.scroll_to_bottom()
-    select_txt = 'a[href*="{}"]'.format(trade_coin_name)
-    sb.assert_element(select_txt)
-    sb.focus(select_txt)
+    selector_txt = 'a[href*="{}"]'.format(trade_coin_name)
+    sb.wait_for_element(selector_txt)
+    sb.focus(selector_txt)
 
 
 @step("click trade button for {trade_coin_name}")
@@ -31,4 +34,5 @@ def click_trade_button(context, trade_coin_name):
 @step("show trade page for {trade_coin_name:S}")
 def check_trade_page(context, trade_coin_name):
     sb = context.sb
+    sb.wait_for_element("div.toggle")
     sb.assert_text(trade_coin_name, "div.toggle")
